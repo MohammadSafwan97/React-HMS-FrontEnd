7bb421008b461b5d3a091362050c31f809508c58
Muhammad Safwan <mohammadsafwan248@gmail.com>
2026-03-13 17:14:38 +0500

    fix appointment api url issue

--- src/feature/appointments/services/appointmentService.js
+++ src/feature/appointments/services/appointmentService.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-const API_BASE="https://spring-boot-hospital-management-system.onrender.com/api/appointments"
+const API_BASE_URL="https://spring-boot-hospital-management-system.onrender.com/api/appointments"
 
 export const getAppointments =async () => {
-  const res=await axios.get(`${API_BASE}/appointments`);
+  const res=await axios.get(`${API_BASE}`);
   return res.data;
 };
 
 export const createAppointment = async(appointment) => {
-  const res=await axios.post(`${API_BASE}/appointments`, appointment);
+  const res=await axios.post(`${API_BASE}`, appointment);
   return res.data;
 };
 
 export const updateAppointment = async(id, appointment) => {
- const res=await axios.put(`${API_BASE}/appointments/${id}`, appointment);
+ const res=await axios.put(`${API_BASE}/${id}`, appointment);
  return res.data;
 };
 
 export const deleteAppointment=async(id)=>{
-  const res= await axios.delete(`${API_BASE}/appointments/${id}`)
+  const res= await axios.delete(`${API_BASE}${id}`)
   return res.data
 }
