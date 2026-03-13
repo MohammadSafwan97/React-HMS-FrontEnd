69a6ba2fbd7f649782ec458635aa001645d2dd36
Muhammad Safwan <mohammadsafwan248@gmail.com>
2026-03-13 16:09:59 +0500

    update api for doctors to connect render backend

---
+++ src/config/api.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL =
+  "https://spring-boot-hospital-management-system.onrender.com/api";
--- src/feature/doctors/services/doctorService.js
+++ src/feature/doctors/services/doctorService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/doctors";
+import { API_BASE_URL } from "../../../config/api.js";
 
 export const createDoctor = async (doctor) => {
 
