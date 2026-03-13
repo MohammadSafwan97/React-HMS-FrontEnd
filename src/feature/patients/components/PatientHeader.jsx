caba7f8ff85116e1015367298b2a1663a00e4019
Muhammad Safwan <mohammadsafwan248@gmail.com>
2026-03-13 17:43:58 +0500

    update patient module

--- src/feature/patients/components/PatientHeader.jsx
+++ src/feature/patients/components/PatientHeader.jsx
@@ -1,6 +1,6 @@
 import { Plus, FileText } from 'lucide-react';
 
-export default function PatientsHeader({ onAdd, onReport }) {
+export default function PatientsHeader({ onAdd }) {
   return (
     <div className="flex justify-between mb-6">
       <h1 className="text-xl font-semibold">
@@ -8,24 +8,7 @@ export default function PatientsHeader({ onAdd, onReport }) {
       </h1>
 
       <div className="flex gap-3">
-         <button
-  onClick={onReport}
-  className="
-    inline-flex items-center gap-2
-    px-5 py-2.5
-    rounded-lg
-    border border-slate-300
-    bg-white
-    text-slate-700
-    text-sm font-medium
-    hover:bg-slate-50
-    focus:outline-none focus:ring-2 focus:ring-slate-400
-    transition
-  "
->
-  <FileText className="w-4 h-4" />
-  Generate Report
-</button>
+        
 
 <button
   onClick={onAdd}
